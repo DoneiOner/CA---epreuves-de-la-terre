659cb87e527cab23e43332650a5af1e6c46ca360
donei <donei.dev@gmail.com>
2022-01-10 22:31:33 +0100

    exo 15

---
+++ terre15.rb
@@ -0,0 +1,19 @@
+#!usr/bin/env ruby
+
+=begin
+   ************************** Terre : ok 
+
+Créez un programme qui célèbre votre victoire.
+
+Exemples d’utilisation :
+$> ruby exo.rb
+J’ai terminé l’Épreuve de la Terre et c’était [].
+$>
+
+Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+
+=end
+
+puts "\n\n Donei Oner a terminé l'épreuve de la Terre et c'était STI-MU-LANT et RAGEANT ... EN-COU-RAGEANT \n  \n"
+puts " (enfin, peut-être un peu moins que la voisine qui me glisse son numéro pour se voir samedi dernier ... \n
+ mais on dit que la vie est une question de priorités et je compte bien remplir mes objectifs dorénavant)\n\n\n"
